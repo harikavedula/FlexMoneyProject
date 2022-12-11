69000b5c0f661e8a7cffce505926e88d3210897b
Aman-Syed <ammuaman256@gmail.com>
2022-12-12 03:15:08 +0530

    a

--- Yoga/Yoga/urls.py
+++ Yoga/Yoga/urls.py
@@ -15,9 +15,10 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path
-from core.views import *
+from core import views
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path("", front, name="front"),
+    path('', views.login),
+
 ]
--- Yoga/core/views.py
+++ Yoga/core/views.py
@@ -3,6 +3,6 @@ from django.shortcuts import render
 # Create your views here.
 
 
-def front(request):
-    context = {}
-    return render(request, "index.html", context)
+def login(request):
+    
+    return render(request, "login.html")
